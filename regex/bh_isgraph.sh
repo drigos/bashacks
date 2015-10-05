3c59dbae0d745a6ccdf96135922f8f4972338bd0
Rodrigo F Martins <martins.drg@gmail.com>
2015-10-05 15:13:45 -0300

    Changed isgraph() to allow string instead of just one character

--- regex/bh_isgraph.sh
+++ regex/bh_isgraph.sh
@@ -1,14 +1,7 @@
 bh_isgraph()
 {
-    local char_in="$1"
+    [ $# -ne 1 ] && return 1
 
-    # dec 33 - 126
-
-    [ "${#char_in}" -gt 2 ] && return 1 
-
-    [ $(bh_asc2dec "${char_in}") -ge 33 -a \
-        $(bh_asc2dec "${char_in}") -le 126 ] &&
-        return 0 
-
-    return 1
+    echo "$1" | grep -Pqw '^[\x21-\x7E]+$'
+    # echo "$1" | grep -Eqw '^[[:graph:]]+$'
 }
