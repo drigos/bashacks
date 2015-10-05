ba483e38a5f9db36ff3b79ef6162fa270da0721a
Rodrigo F Martins <martins.drg@gmail.com>
2015-10-05 15:44:48 -0300

    Fix isprint(): input string formatting was wrong; character 127 is unprintable

--- regex/bh_isprint.sh
+++ regex/bh_isprint.sh
@@ -3,8 +3,8 @@ bh_isprint()
 	# nao ta rolando
 	local i
 
-	for i in $(bh_str2hex -0x "$1" | sed 's/\(....\)/\1 /g'); do
-		[ $(($i)) -ge 32 -a $(($i)) -le 127 ] || return 1
+	for i in $(bh_str2hex -0 "$1"); do
+		[ $(($i)) -ge 32 -a $(($i)) -le 126 ] || return 1
 	done
 	return 0
 }
