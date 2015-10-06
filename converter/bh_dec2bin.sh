dfe678c4dadee79b429acec19ecbf57286552181
Rodrigo F Martins <martins.drg@gmail.com>
2015-10-05 23:32:59 -0300

    Changed dec2bin() to allow multiple arguments

--- converter/bh_dec2bin.sh
+++ converter/bh_dec2bin.sh
@@ -1,6 +1,13 @@
 bh_dec2bin()
 {
-    [ $# -eq 0 ] && return 1
+    [ ${#} -eq 0 ] && return 1
 
-    echo "obase=2;$1" | bc
+    local bin
+
+	for i in ${*}; do
+        bin=$(echo "obase=2;${i}" | bc)
+		echo -n "${bin} "
+    done
+
+	echo
 }
