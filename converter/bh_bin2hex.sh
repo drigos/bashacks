7307618a6b7d628eab9a0d759c9d5c63ee2e638a
Rodrigo F Martins <martins.drg@gmail.com>
2015-10-05 23:20:03 -0300

    Changed bin2hex() to allow multiple arguments

--- converter/bh_bin2hex.sh
+++ converter/bh_bin2hex.sh
@@ -1,13 +1,13 @@
 bh_bin2hex()
 {
-    [ $# -eq 0 ] && return 1
+    [ ${#} -eq 0 ] && return 1
 
-	local hex
-	local i
+    local hex
 
-	for i in $*; do
-		hex=$(echo "obase=16;ibase=2;$(echo $i)" | bc)
-		echo -n "$hex "
+	for i in ${*}; do
+		hex=$(echo "obase=16;ibase=2;${i}" | bc)
+		echo -n "${hex} "
     done
+
 	echo
 }
